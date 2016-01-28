8e90939885d21839243ca758433d28fd9c1abb25
Benjamin Flores <beneliflo@gmail.com>
2016-01-28 20:56:47 -0300

    custom select

--- src/components/editor/scheduleEditor.js
+++ src/components/editor/scheduleEditor.js
@@ -7,11 +7,11 @@ import ToggleButton from 'components/toggleButton';
 export default class ScheduleEditor extends React.Component {
     constructor(props) {
         super(props);
-        
+
         const now = new Date();
         const frequencyMetric = 'mins';
         const frequencyValue = 10;
-        
+
         this.state = {
             advanced: !!props.schedule,
             currentDate: now,
@@ -21,22 +21,22 @@ export default class ScheduleEditor extends React.Component {
             schedule: props.schedule || this.createIntervalSchedule(now, frequencyMetric, frequencyValue),
         };
     }
-    
+
     componentDidMount() {
         this.nowInterval = setInterval(() => this.setState({ now: new Date() }), 1000 * 60);
-        
+
         // Report any default schedule settings to parent
         this.onChangeSchedule(this.state.schedule);
     }
-    
+
     componentWillUnmount() {
         clearInterval(this.nowInterval);
     }
-    
+
     render() {
         const schedule = this.getValue();
         let nextRun;
-        
+
         try {
             const cron = Cron.parseExpression(schedule, {
                 currentDate: this.state.currentDate,
@@ -44,15 +44,15 @@ export default class ScheduleEditor extends React.Component {
             const next = cron.next();
             const startOfToday = new Date(this.state.now.valueOf());
             const days = 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split(' ');
-            
+
             startOfToday.setHours(0);
             startOfToday.setMinutes(0);
             startOfToday.setSeconds(0);
             startOfToday.setMilliseconds(0);
-            
+
             const deltaMinutes = (next.valueOf() - startOfToday.valueOf()) / (1000 * 60);
             let day;
-            
+
             if (deltaMinutes < 60 * 24) {
                 day = 'today';
             } else if (deltaMinutes < 60 * 24 * 2) {
@@ -64,18 +64,18 @@ export default class ScheduleEditor extends React.Component {
             } else {
                 day = 'in ' + Math.floor(deltaMinutes / (60 * 24)) + ' days';
             }
-            
+
             nextRun = {
                 day: day,
                 time: next.toLocaleTimeString().replace(':00 ', ' '), // Strip seconds
             };
-            
+
         } catch (e) { }
-        
+
         const metrics = Object.keys(ScheduleEditor.frequencyMetrics);
         const allowed = ScheduleEditor.frequencyMetrics[this.state.frequencyMetric].allowed;
         const frequencyValueLabel = allowed[this.state.frequencyValue];
-        
+
         return (
             <div className="a0-schedule-pane">
                 <div className="a0-schedule-display">
@@ -106,45 +106,49 @@ export default class ScheduleEditor extends React.Component {
                     <span className="a0-inline-text -inverted -bright">Run this every</span>
                     { Object.keys(allowed).length > 1
                     ?   (
-                            <select
-                                className="a0-value"
-                                disabled={ this.state.advanced }
-                                title={ frequencyValueLabel }
-                                onChange={ (e) => this.onChangeFrequencyValue(e.target.value) }
-                                id="frequencyValue"
-                                value={ this.state.frequencyValue }
-                            >
-                                {
-                                    Object.keys(allowed).map(i => (
-                                        <option
-                                            eventKey={ i }
-                                            key={ i }
-                                            value={ i }
-                                        >{ allowed[i] }</option>
-                                    ))
-                                }
-                            </select>
+                            <div className="a0-custom-select">
+                                <select
+                                    className="a0-value"
+                                    disabled={ this.state.advanced }
+                                    title={ frequencyValueLabel }
+                                    onChange={ (e) => this.onChangeFrequencyValue(e.target.value) }
+                                    id="frequencyValue"
+                                    value={ this.state.frequencyValue }
+                                >
+                                    {
+                                        Object.keys(allowed).map(i => (
+                                            <option
+                                                eventKey={ i }
+                                                key={ i }
+                                                value={ i }
+                                            >{ allowed[i] }</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
                         )
                     :   null
                     }
-                    <select
-                        className="a0-metric"
-                        disabled={ this.state.advanced }
-                        title={ this.state.frequencyMetric }
-                        onChange={ (e) => this.onChangeFrequencyMetric(e.target.value) }
-                        id="frequencyMetric"
-                        value={ this.state.frequencyMetric }
-                    >
-                        {
-                            metrics.map(value => (
-                                <option
-                                    eventKey={ value }
-                                    key={ value }
-                                    value={ value }
-                                >{ value }</option>
-                            ))
-                        }
-                    </select>
+                    <div className="a0-custom-select">
+                        <select
+                            className="a0-metric"
+                            disabled={ this.state.advanced }
+                            title={ this.state.frequencyMetric }
+                            onChange={ (e) => this.onChangeFrequencyMetric(e.target.value) }
+                            id="frequencyMetric"
+                            value={ this.state.frequencyMetric }
+                        >
+                            {
+                                metrics.map(value => (
+                                    <option
+                                        eventKey={ value }
+                                        key={ value }
+                                        value={ value }
+                                    >{ value }</option>
+                                ))
+                            }
+                        </select>
+                    </div>
                 </div>
                 <div className="a0-advanced-cron">
                     <label>
@@ -164,7 +168,7 @@ export default class ScheduleEditor extends React.Component {
             </div>
         );
     }
-    
+
     createIntervalSchedule(d, frequencyMetric, frequencyValue) {
         // How does this work, you may ask
         //
@@ -176,14 +180,14 @@ export default class ScheduleEditor extends React.Component {
         // 2.  If the current position in the cron string array is lower than the current
         //     metric's offset, we leave that part of the cron string as is because it
         //     represents 'right now'
-        // 3.  Otherwise, we want to build a list of hour/minute offsets that correspond to 
+        // 3.  Otherwise, we want to build a list of hour/minute offsets that correspond to
         //     intervals of `frequencyValue` units of `frequencyMetric` from the current time.
         const nowSchedule = [d.getMinutes(), d.getHours(), d.getDate(), d.getMonth() + 1, d.getDay()];
         const metric = ScheduleEditor.frequencyMetrics[frequencyMetric];
         const freeze = typeof metric.freeze === 'undefined'
             ?   metric.offset
             :   metric.freeze;
-        
+
         const schedule = metric
             ?   nowSchedule.map((curr, pos) =>
                     pos === metric.offset
@@ -193,55 +197,55 @@ export default class ScheduleEditor extends React.Component {
                             :   curr
                 )
             :   nowSchedule;
-        
+
         return schedule.join(' ');
     }
-    
+
     getValue() {
         return this.state.schedule;
     }
-    
+
     onChangeFrequencyMetric(frequencyMetric) {
         const metric = ScheduleEditor.frequencyMetrics[frequencyMetric];
         const currentDate = new Date();
-        
+
         if (!metric) return;
-        
+
         let frequencyValue = this.state.frequencyValue;
-        
+
         if (!metric.allowed[frequencyValue]) {
             frequencyValue = Object.keys(metric.allowed)[0];
         }
-        
+
         const schedule = this.createIntervalSchedule(currentDate, frequencyMetric, frequencyValue);
-        
+
         this.setState({ frequencyMetric, frequencyValue, currentDate, schedule }, () => {
             if (this.props.onChangeSchedule) this.props.onChangeSchedule(this.getValue());
         });
     }
-    
+
     onChangeFrequencyValue(frequencyValue) {
         const currentDate = new Date();
         const schedule = this.createIntervalSchedule(currentDate, this.state.frequencyMetric, frequencyValue);
-        
+
         this.setState({ frequencyValue, currentDate, schedule }, () => {
             if (this.props.onChangeSchedule) this.props.onChangeSchedule(this.getValue());
         });
     }
-    
+
     onChangeSchedule(schedule) {
         const currentDate = new Date();
-        
+
         this.setState({ schedule, currentDate }, () => {
             this.props.onChangeSchedule(this.getValue());
         });
     }
-    
+
     onChangeState(state) {
         const newState = state ? 'active' : 'inactive';
-        
+
         console.log('ScheduleEditor.onChangeState', state, newState);
-        
+
         this.props.onChangeState(newState);
     }
 }
@@ -253,7 +257,7 @@ ScheduleEditor.frequencyMetrics = {
         offset: 0,
         encode: (curr, frequencyValue, metric) => {
             const mod = curr % frequencyValue;
-            
+
             return mod > 0
                 ?   `${curr % frequencyValue}-${metric.max - 1}/${frequencyValue}`
                 :   `*/${frequencyValue}`;
@@ -265,7 +269,7 @@ ScheduleEditor.frequencyMetrics = {
         offset: 1,
         encode: (curr, frequencyValue, metric) => {
             const mod = curr % frequencyValue;
-            
+
             return mod > 0
                 ?   `${curr % frequencyValue}-${metric.max - 1}/${frequencyValue}`
                 :   `*/${frequencyValue}`;
