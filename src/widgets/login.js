d62b2c843a8262563fc06797de3ccb5c32ca5c73
Geoffrey Goodman <ggoodman@gmail.com>
2016-02-23 11:42:56 -0500

    Expose login and cancel events on login widget

--- src/widgets/login.js
+++ src/widgets/login.js
@@ -3,6 +3,12 @@ import Widget from 'lib/widget';
 
 export default class LoginWidget extends Widget {
     constructor(props) {
-        super(Login, props);
+        super(Login, {
+            events: {
+                login: 'onLogin',
+                cancel: 'onCancel',
+            },
+            ...props
+        });
     }
 }
